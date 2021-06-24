3a6b2e824fe299302dfdb20bfce406e48b43886c
andrewtookay <andrewtookay@gmail.com>
2021-06-24 03:01:28 +0300

    updated chia_bls.mk

--- depends/packages/chia_bls.mk
+++ depends/packages/chia_bls.mk
@@ -3,7 +3,8 @@ $(package)_version=v20181101
 # It's actually from https://github.com/Chia-Network/bls-signatures, but we have so many patches atm that it's forked
 $(package)_download_path=https://github.com/dashpay/bls-signatures/archive 
 # TODO_ADOT_MEDIUM create Alterdot bls-signatures repo on GitHub
-$(package)_file_name=$($(package)_version).tar.gz
+$(package)_download_file=$($(package)_version).tar.gz
+$(package)_file_name=$(package)-$($(package)_download_file)
 $(package)_sha256_hash=b3ec74a77a7b6795f84b05e051a0824ef8d9e05b04b2993f01040f35689aa87c
 $(package)_dependencies=gmp
 #$(package)_patches=...TODO (when we switch back to https://github.com/Chia-Network/bls-signatures)
